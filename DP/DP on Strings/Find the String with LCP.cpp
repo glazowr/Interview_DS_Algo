7796bcc8c66397245257c49b7719cfc865c35a3f
MAZHAR <iiestshibpur605@gmail.com>
2026-03-28 07:08:21 +0530

    added one more way

--- DP/DP on Strings/Find the String with LCP.cpp
+++ DP/DP on Strings/Find the String with LCP.cpp
@@ -7,7 +7,7 @@
 
 
 /****************************************************************** C++ **********************************************************/
-//Approach (Using Greedy Allocation + Comparing LCP)
+//Approach-1 (Using Greedy Allocation + Finding LCP and comparing)
 //T.C : O(n^2)
 //S.C : O(n^2)
 class Solution {
@@ -59,6 +59,89 @@ public:
         
         if (LongestCommonPrefixMatrix(result) == lcp) return result;
         return "";
+    }
+};
+
+
+
+//Approach-2 (Using Greedy Allocation + Without finding LCP)
+//T.C : O(n^2)
+//S.C : O(n) for word
+class Solution {
+public:
+    bool checkLCP(string &word, vector<vector<int>>& lcp) {
+        int n = word.length();
+
+        for(int i = 0; i < n; i++) {
+            //lcp[i][n-1] = (word[i] == word[n-1]) ? 1 : 0;
+            if(word[i] != word[n-1]) {
+                if(lcp[i][n-1] != 0) return false;
+            } else {
+                if(lcp[i][n-1] != 1) return false;
+            }
+        }
+
+        for(int j = 0; j < n; j++) {
+            //lcp[n-1][j] = (word[n-1] == word[j]) ? 1 : 0;
+            if(word[n-1] != word[j]) {
+                if(lcp[n-1][j] != 0) return false;
+            } else {
+                if(lcp[n-1][j] != 1) return false;
+            }
+        }
+
+        for(int i = n-2; i >= 0; i--) {
+            for(int j = n-2; j >= 0; j--) {
+                if(word[i] == word[j]) {
+                    if(lcp[i][j] != 1 + lcp[i+1][j+1]) return false;
+                } else {
+                    if(lcp[i][j] != 0) return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    string findTheString(vector<vector<int>>& lcp) {
+        int n = lcp.size();
+
+        string word(n, '$');
+
+        for(int i = 0; i < n; i++) {
+
+            //word[i] = ?
+            for(int j = 0; j < i; j++) {
+                if(lcp[j][i] != 0) {
+                    word[i] = word[j];
+                    break;
+                }
+            }
+
+
+            if(word[i] == '$') {
+                vector<bool> forbidden(26, false);
+
+                for(int j = 0; j < i; j++) {
+                    if(lcp[j][i] == 0) {
+                        forbidden[word[j] - 'a'] = true;
+                    }
+                }
+
+                //a, b, c ..., z
+                for(int idx = 0; idx < 26; idx++) {
+                    if(forbidden[idx] == false) {
+                        word[i] = idx + 'a';
+                        break;
+                    }
+                }
+
+                if(word[i] == '$')
+                    return ""; //we could never fill word[i]
+            }
+        }
+
+        return checkLCP(word, lcp) == true ? word : "";
     }
 };
 
@@ -155,5 +238,93 @@ class Solution {
         }
 
         return result;
+    }
+}
+
+
+
+//Approach-2 (Using Greedy Allocation + Without finding LCP)
+//T.C : O(n^2)
+//S.C : O(n) for word
+class Solution {
+
+    public boolean checkLCP(String word, int[][] lcp) {
+        int n = word.length();
+
+        // Last column
+        for (int i = 0; i < n; i++) {
+            if (word.charAt(i) != word.charAt(n - 1)) {
+                if (lcp[i][n - 1] != 0) return false;
+            } else {
+                if (lcp[i][n - 1] != 1) return false;
+            }
+        }
+
+        // Last row
+        for (int j = 0; j < n; j++) {
+            if (word.charAt(n - 1) != word.charAt(j)) {
+                if (lcp[n - 1][j] != 0) return false;
+            } else {
+                if (lcp[n - 1][j] != 1) return false;
+            }
+        }
+
+        // Remaining cells
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                if (word.charAt(i) == word.charAt(j)) {
+                    if (lcp[i][j] != 1 + lcp[i + 1][j + 1]) return false;
+                } else {
+                    if (lcp[i][j] != 0) return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public String findTheString(int[][] lcp) {
+        int n = lcp.length;
+
+        char[] word = new char[n];
+        for (int i = 0; i < n; i++) {
+            word[i] = '$';
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            // Try to match with previous
+            for (int j = 0; j < i; j++) {
+                if (lcp[j][i] != 0) {
+                    word[i] = word[j];
+                    break;
+                }
+            }
+
+            // Assign smallest valid character
+            if (word[i] == '$') {
+                boolean[] forbidden = new boolean[26];
+
+                for (int j = 0; j < i; j++) {
+                    if (lcp[j][i] == 0) {
+                        forbidden[word[j] - 'a'] = true;
+                    }
+                }
+
+                for (int idx = 0; idx < 26; idx++) {
+                    if (!forbidden[idx]) {
+                        word[i] = (char) ('a' + idx);
+                        break;
+                    }
+                }
+
+                if (word[i] == '$') {
+                    return "";
+                }
+            }
+        }
+
+        String result = new String(word);
+        return checkLCP(result, lcp) ? result : "";
     }
 }
